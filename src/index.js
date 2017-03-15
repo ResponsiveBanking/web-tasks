2b4d3e3f59ef7c4969cb8172c75fdc1bbc203f5f
DaisyT1 <daisyatucker@gmail.com>
2017-03-15 10:47:40 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import ConfirmComponent from './components/Confirm';
 import NotificationComponent from './components/Notification';
 import Challange1Component from './components/Challange1';
+import OverspendComponent from './components/Overspend';
 
 import { Router, Route, hashHistory } from 'react-router';
 
@@ -13,6 +14,7 @@ const Root = () => (
             <Route path="notification" component={NotificationComponent} />
             <Route path="confirm" component={ConfirmComponent} />
             <Route path="challange1" component={Challange1Component} />
+            <Route path="overspend" component={OverspendComponent} />
         </Route>
     </Router>
 );
